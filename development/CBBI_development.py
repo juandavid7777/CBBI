97b8fb5004f7fc27634012b1010638256517a5e9
juandavid7777 <juandavid7777@gmail.com>
2024-01-05 13:11:42 +0400

    Update CBBI_development.py

--- development/CBBI_development.py
+++ development/CBBI_development.py
@@ -43,3 +43,35 @@ df
 
 #----------------------------------------------------------------------
 df.drop(columns = ["Price"]).plot()
+
+
+#----------------------------------------------------------------------
+def cs_fetch(path: str, data_selector: str, col_name: str) -> pd.DataFrame:
+    response = HTTP.get(f'https://coinsoto.com/indicatorapi/{path}')
+    response.raise_for_status()
+    data = response.json()['data']
+
+    if 'timeList' not in data and 'line' in data:
+        data = data['line']
+
+    data_x = data['timeList']
+    data_y = data[data_selector]
+    assert len(data_x) == len(data_y), f'{len(data_x)=} != {len(data_y)=}'
+
+    df = pd.DataFrame({
+        'Date': data_x[:len(data_y)],
+        col_name: data_y,
+    })
+
+    df['Date'] = pd.to_datetime(df['Date'], unit='ms').dt.tz_localize(None)
+
+    return df
+
+# Example cs_fetch ---------------------------------------------------
+df = cs_fetch(
+            path='chain/index/charts?type=/charts/mvrv-zscore/',
+            data_selector='value4',
+            col_name='MVRV'
+).set_index("Date")
+
+df.plot(log_y = False)
